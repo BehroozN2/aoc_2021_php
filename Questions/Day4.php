999a67764ace444af85c382a9fc3490847ae6359
Behrooz <42407905+BehroozN2@users.noreply.github.com>
2021-12-04 07:57:55 +0100

    Day 4

---
+++ Questions/Day4.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Questions;
+
+class Day4 extends AbstractQuestion
+{
+    protected function part1(): string
+    {
+        [$numbers, $boards] = $this->parseInput();
+
+        foreach ($numbers as $number) {
+            foreach ($boards as $board) {
+                /** @var Day4Board $board */
+                $board->mark($number);
+
+                if ($board->hasWon()) {
+                    return (string)($board->getUnmarkedSum() * $number);
+                }
+            }
+        }
+    }
+
+    protected function part2(): string
+    {
+        [$numbers, $boards] = $this->parseInput();
+
+        $lastWonBoardUnmarkedSum = 0;
+
+        foreach ($numbers as $number) {
+            foreach ($boards as &$board) {
+                /** @var Day4Board $board */
+                $board->mark($number);
+
+                if ($board->hasWon()) {
+                    $lastWonBoardUnmarkedSum = $board->getUnmarkedSum();
+                    $board = null;
+                }
+            }
+
+            $boards = array_filter($boards);
+
+            if (count($boards) === 0) {
+                return (string)($lastWonBoardUnmarkedSum * $number);
+            }
+        }
+    }
+
+    protected function parseInput(): array
+    {
+        $input = $this->input;
+
+        $numbers = array_map(
+            fn($integer) => (int)$integer,
+            explode(
+                ',',
+                array_shift($input),
+            ),
+        );
+
+        $boards = array_map(
+            fn($board) => new Day4Board($board),
+            array_chunk(
+                array_map(
+                    fn($row) => array_map(
+                        fn($integer) => (int)trim($integer),
+                        str_split($row, 3),
+                    ),
+                    array_filter($input),
+                ),
+                5,
+            ),
+        );
+
+        return [$numbers, $boards];
+    }
+}
+
+class Day4Board
+{
+    protected array $marked;
+    protected int $rowsCount;
+    protected int $columnsCount;
+
+    public function __construct(protected array $board)
+    {
+        $this->rowsCount = count($this->board);
+        $this->columnsCount = count($this->board[0]);
+
+        $this->marked = array_fill(
+            0,
+            $this->rowsCount,
+            array_fill(
+                0,
+                $this->columnsCount,
+                0,
+            ),
+        );
+    }
+
+    public function mark(int $number)
+    {
+        foreach ($this->board as $rowPosition => $row) {
+            foreach ($row as $columnPosition => $column) {
+                if ($number === $column) {
+                    $this->marked[$rowPosition][$columnPosition] = 1;
+                }
+            }
+        }
+    }
+
+    public function hasWon(): bool
+    {
+        foreach ($this->marked as $markedRow) {
+            if (array_sum($markedRow) === $this->rowsCount) {
+                return true;
+            }
+        }
+
+        for ($columnPosition = 0; $columnPosition < $this->columnsCount; $columnPosition++) {
+            $markedColumn = array_column($this->marked, $columnPosition);
+
+            if (array_sum($markedColumn) === $this->columnsCount) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function getUnmarkedSum(): int
+    {
+        $unmarkedSum = 0;
+
+        foreach ($this->marked as $rowPosition => $markedRow) {
+            foreach ($markedRow as $columnPosition => $markedColumn) {
+                if ($markedColumn === 0) {
+                    $unmarkedSum += $this->board[$rowPosition][$columnPosition];
+                }
+            }
+        }
+
+        return $unmarkedSum;
+    }
+}
